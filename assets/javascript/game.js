93f2c1d9b08335115f3f02d0eeb5d6d7238ab870
Terence Ro <terencero@dhcp-10-102-236-249.wireless.northwestern.private>
2016-10-17 09:47:31 -0500

    updated javascript, add object, onkeyup, global var keys

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+ 
+
+
+
+
+ var keys = ['s', 'o', 'c', 'r', 'a', 't', 'e', 'p', 'l', 'i', 'd', 'n', 'z', 'h'];
+// Captures Key Clicks
+    document.onkeyup = function(event) {
+// Determines which exact key was selected. Make it lowercase
+	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+// object variable containing words and answers
+
+var currentWord {
+	word1: 'socrates',
+	word2: 'plato',
+	word3: 'aristotle',
+	word4: 'descartes',
+	word5: 'nietzsche'
+}
+
+
+} // end of document.onkeyup 
