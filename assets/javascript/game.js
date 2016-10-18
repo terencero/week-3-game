d0f579c28e75001ae975eac5233c8bc19374706b
Terence Ro <terencero@dhcp-10-105-116-215.wireless.northwestern.private>
2016-10-17 22:07:23 -0500

    update js to reflect arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,58 @@
  
-
-
-
-
  var keys = ['s', 'o', 'c', 'r', 'a', 't', 'e', 'p', 'l', 'i', 'd', 'n', 'z', 'h'];
 // Captures Key Clicks
     document.onkeyup = function(event) {
 // Determines which exact key was selected. Make it lowercase
 	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 
-// object variable containing words and answers
 
-var currentWord {
-	word1: 'socrates',
-	word2: 'plato',
-	word3: 'aristotle',
-	word4: 'descartes',
-	word5: 'nietzsche',
+
+
+// array containing words and answers
+
+var wordsArray = [
+	'socrates',
+	'plato',
+	'aristotle',
+	'descartes',
+	'nietzsche'
+]
+
+var result = Math.floor((Math.random() * wordsArray.length));
+
+var word = wordsArray[result]
+
+
+
+
+
+	// var socrates = document.getElementsByClassName('word-line');
+	
+	// socrates.innerHTML = '_ _ _ _ _ _ _ _'
+
+	// if (userInput === 's'){
+
+	// }
 
 	
-}
 
+
+
+// function()
+
+// var dash = document.getElementsByClassName('word-line');
+
+// dash = innerHTML.replace("_ _ _ _ _ _ _ _");
+
+// function setDashes(){
+
+// 		console.log(dash);
+
+		//dash.innerHTML.replace("_ _ _ _ _ _ _ _");
+	// }
+
+if (userInput === 's') {
+	console.log(dash);
+}
 
 } // end of document.onkeyup 
